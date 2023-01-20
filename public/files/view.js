20feb2aaf02638c9cfd60e6ce69f3e834f26d100
Obsidian_Seal <bmgosselin@gmail.com>
2023-01-20 14:24:26 -0500

    views done?

--- public/files/view.js
+++ public/files/view.js
@@ -8,7 +8,32 @@ const firebaseConfig = {
 	appId: "1:636989973689:web:6d938d9588606d803a6768",
 };
 
-// init app here
-// TODO: firebase imports on all posts
+firebase.initializeApp(firebaseConfig);
 
-const postName = document.querySelector("h3").innerHTML;
+const illegalCharacters = [".", "#", "$", "[", "]", "/", "\\"];
+var postName = document.querySelector("h3").innerHTML;
+illegalCharacters.forEach((character) => (postName = postName.replace(character, "_")));
+var alreadyViewed = false;
+
+const postViewsRef = firebase.database().ref("/post-views/" + postName);
+postViewsRef.on("value", (snapshot) => {
+	if (snapshot.exists()) {
+		const views = snapshot.val().views;
+		addView(views);
+	} else {
+		addView(0);
+	}
+});
+
+function addView(views) {
+	if (!alreadyViewed) {
+		alreadyViewed = true;
+
+		firebase
+			.database()
+			.ref("/post-views/" + postName)
+			.set({
+				views: views + 1,
+			});
+	}
+}
