510f79c6363e2be33069e81159327673452d023b
ObsidianSeal <bmgosselin@gmail.com>
2023-01-19 23:43:05 -0500

    start view.js

---
+++ public/files/view.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyAigBAgn7Uhybip_m3P508DIpybb2qdI5g",
+	authDomain: "the-pinniped-page.firebaseapp.com",
+	databaseURL: "https://the-pinniped-page.firebaseio.com",
+	projectId: "the-pinniped-page",
+	storageBucket: "the-pinniped-page.appspot.com",
+	messagingSenderId: "636989973689",
+	appId: "1:636989973689:web:6d938d9588606d803a6768",
+};
+
+// init app here
+// TODO: firebase imports on all posts
+
+const postName = document.querySelector("h3").innerHTML;
