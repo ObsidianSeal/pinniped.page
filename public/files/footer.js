d3e9ee3133fdc5de1aa684613ab71e3a7fb7dc3b
ObsidianSeal <bmgosselin@gmail.com>
2023-09-10 21:34:28 -0400

    tag +-, tag html format change, footer debug?, brb v10

--- public/files/footer.js
+++ public/files/footer.js
@@ -8,6 +8,7 @@ const screenHeight = window.innerHeight;
 if (screenHeight - 80 > contentBottom) {
 	const space = document.createElement("div");
 	const height = screenHeight - 80 - contentBottom;
+	console.log(`DEBUG: added a ${height}px tall space to the bottom of this page.`);
 	space.setAttribute("style", `height: ${height}px`);
 	content.appendChild(space);
 }
