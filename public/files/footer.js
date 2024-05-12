cd688d9df26d1f851bc665265d3b38ed73a3aed9
ObsidianSeal <bmgosselin@gmail.com>
2024-05-12 00:55:11 -0400

    prevous and next links, measure changes, AND MUCH MORE (WOW: total 50 files changed)

--- public/files/footer.js
+++ public/files/footer.js
@@ -1,12 +1,13 @@
 const content = document.querySelector(".content");
-const contentEnd = document.querySelector(".content-end");
-const contentBottom = contentEnd.getBoundingClientRect().bottom;
+const contentBottom = content.getBoundingClientRect().bottom;
 const screenHeight = window.innerHeight;
 
-if (screenHeight - 80 > contentBottom) {
-	const space = document.createElement("div");
-	const height = screenHeight - 80 - contentBottom;
-	console.log(`DEBUG: added a ${height}px tall space to the bottom of this page.`);
-	space.setAttribute("style", `height: ${height}px`);
-	content.appendChild(space);
-}
+document.addEventListener("DOMContentLoaded", () => {
+	if (screenHeight - 80 > contentBottom) {
+		const space = document.createElement("div");
+		const height = screenHeight - 80 - contentBottom;
+		console.log(`DEBUG: added a ${height}px tall space to the bottom of this page.`);
+		space.setAttribute("style", `height: ${height}px`);
+		content.appendChild(space);
+	}
+});
--- public/files/to-top.js
+++ public/files/to-top.js
@@ -5,12 +5,12 @@ toTopButton.innerHTML = '<i class="fa-solid fa-arrow-up-long"></i>';
 document.querySelector(".content").appendChild(toTopButton);
 
 // to-top button scroll
-toTopButton.addEventListener("click", (e) => {
+toTopButton.addEventListener("click", () => {
 	window.scroll(0, 0);
 });
 
 // show/hide to-top button
-document.addEventListener("scroll", (e) => {
+document.addEventListener("scroll", () => {
 	if (window.scrollY > 300) toTopButton.style = "display: flex";
 	else toTopButton.style = "display: none";
 });
