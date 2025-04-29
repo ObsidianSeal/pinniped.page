5756a9050934742de9baa3ec956b20e647565847
ObsidianSeal <bmgosselin@gmail.com>
2025-04-29 14:53:35 -0400

    playlists updates, including smooth scrolling, now also used in to-top buttons all over ++

--- public/files/to-top.js
+++ public/files/to-top.js
@@ -1,16 +1,19 @@
 const toTopButton = document.createElement("div");
 toTopButton.id = "to_top_button";
-toTopButton.style = "display: none";
+toTopButton.style = "pointer-events: none; opacity: 0";
 toTopButton.innerHTML = '<i class="fa-solid fa-arrow-up-long"></i>';
 document.querySelector(".content").appendChild(toTopButton);
 
 // to-top button scroll
 toTopButton.addEventListener("click", () => {
-	window.scroll(0, 0);
+	window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 });
 
 // show/hide to-top button
 document.addEventListener("scroll", () => {
-	if (window.scrollY > 300) toTopButton.style = "display: flex";
-	else toTopButton.style = "display: none";
+	if (window.scrollY > 300) {
+		toTopButton.style = "pointer-events: all; opacity: 1";
+	} else {
+		toTopButton.style = "pointer-events: none; opacity: 0";
+	}
 });
