a52516694d17949afbf5cfa3afd158583071fa85
ObsidianSeal <bmgosselin@gmail.com>
2025-05-24 01:06:06 -0400

    about text changes, footer.js changes all around, both blog pages get some changes, projects changes, blog post & projects header consistency, bus data pdf metadata fix, etc.

--- public/files/footer.js
+++ public/files/footer.js
@@ -3,11 +3,11 @@ const content = contentElements[contentElements.length - 1];
 const contentBottom = content.getBoundingClientRect().bottom;
 const screenHeight = window.innerHeight;
 
-document.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
 	if (screenHeight - 80 > contentBottom) {
 		const space = document.createElement("div");
 		const height = screenHeight - 80 - contentBottom;
-		console.log(`DEBUG: added a ${height}px tall space to the bottom of this page.`);
+		// console.log(`DEBUG: added a ${height}px tall space to the bottom of this page.`);
 		space.setAttribute("style", `height: ${height}px`);
 		content.appendChild(space);
 	}
